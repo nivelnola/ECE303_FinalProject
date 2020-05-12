e7abb9e82daa16f0dbdaac99813831f23fe47bcf
nivelnola <alon.s.levin@gmail.com>
2020-05-12 03:39:14 -0400

    very little debugging. many errors thrown

--- sender.py
+++ sender.py
@@ -9,36 +9,36 @@ import sys
 
 class Sender(object):
 
-    def __init__(self, inbound_port=50006, outbound_port=50005, timeout=10, debug_level=logging.INFO):
-        self.logger = utils.Logger(self.__class__.__name__, debug_level)
+	def __init__(self, inbound_port=50006, outbound_port=50005, timeout=10, debug_level=logging.INFO):
+		self.logger = utils.Logger(self.__class__.__name__, debug_level)
 
-        self.inbound_port = inbound_port
-        self.outbound_port = outbound_port
-        self.simulator = channelsimulator.ChannelSimulator(inbound_port=inbound_port, outbound_port=outbound_port,
-                                                           debug_level=debug_level)
-        self.simulator.sndr_setup(timeout)
-        self.simulator.rcvr_setup(timeout)
+		self.inbound_port = inbound_port
+		self.outbound_port = outbound_port
+		self.simulator = channelsimulator.ChannelSimulator(inbound_port=inbound_port, outbound_port=outbound_port,
+														   debug_level=debug_level)
+		self.simulator.sndr_setup(timeout)
+		self.simulator.rcvr_setup(timeout)
 
-    def send(self, data):
-        raise NotImplementedError("The base API class has no implementation. Please override and add your own.")
+	def send(self, data):
+		raise NotImplementedError("The base API class has no implementation. Please override and add your own.")
 
 
 class BogoSender(Sender):
 
-    def __init__(self):
-        super(BogoSender, self).__init__()
+	def __init__(self):
+		super(BogoSender, self).__init__()
 
-    def send(self, data):
-        self.logger.info("Sending on port: {} and waiting for ACK on port: {}".format(self.outbound_port, self.inbound_port))
-        while True:
-            try:
-                self.simulator.u_send(data)  # send data
-                ack = self.simulator.u_receive()  # receive ACK
-                self.logger.info("Got ACK from socket: {}".format(
-                    ack.decode('ascii')))  # note that ASCII will only decode bytes in the range 0-127
-                break
-            except socket.timeout:
-                pass
+	def send(self, data):
+		self.logger.info("Sending on port: {} and waiting for ACK on port: {}".format(self.outbound_port, self.inbound_port))
+		while True:
+			try:
+				self.simulator.u_send(data)  # send data
+				ack = self.simulator.u_receive()  # receive ACK
+				self.logger.info("Got ACK from socket: {}".format(
+					ack.decode('ascii')))  # note that ASCII will only decode bytes in the range 0-127
+				break
+			except socket.timeout:
+				pass
 
 ############
 class ReliableSender(Sender):
@@ -48,6 +48,8 @@ class ReliableSender(Sender):
 		:param starting_packet_num: starting packet number for packet numbers
 		:param timeout: timeout of sender to trigger resend, may use later
 		'''
+		super(ReliableSender, self).__init__()
+		
 		self.timeout = timeout
 		self.packet_num = starting_packet_num
 		self.simulator.sndr_socket.settimeout(timeout)
@@ -62,7 +64,7 @@ class ReliableSender(Sender):
 		# Send the data and wait for a good ACK
 		while True: 
 			try:
-				# Send 3 time
+				# Send 3 times
 				for i in range(0,3): 
 					self.simulator.u_send(byte_datagram)
 	
@@ -102,7 +104,7 @@ class DataGram(object):
 		return result
 	
 if __name__ == "__main__":
-    # test out BogoSender
-    DATA = bytearray(sys.stdin.read())
-    sndr = BogoSender()
-    sndr.send(DATA)
+	# test out BogoSender
+	DATA = bytearray(sys.stdin.read())
+	sndr = ReliableSender()
+	sndr.send(DATA)
